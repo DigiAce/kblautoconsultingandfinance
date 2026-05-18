9de52c0c111bc30a18829b685e44deaa899a1e1d
DigiAce <165820914+DigiAce@users.noreply.github.com>
2026-05-19 00:49:16 +0530

    Import Posterpage component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Posterpage "./pages/Posterpage";
 import Contact from "./pages/Contact";
 import Layout from "./components/Layout";
 // import Loans from "./pages/ServicePage/Loans";
